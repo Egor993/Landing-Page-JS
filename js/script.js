86646beaab61a54a4c77ed2478ff3afa13ec369b
Egor Cherepanov <feda9938765@yandex.ru>
2021-01-08 22:27:21 +0300

    'timer'

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+	// Tabs
 	const tabs = document.querySelectorAll('.tabheader__item'),
 	tabsContent = document.querySelectorAll('.tabcontent'),
 	tabsParent = document.querySelector('.tabheader__items');
-
-	// console.log(tabs[1]);
-	// tabsContent[0].style.display = 'block';
-	// tabs[2].classList.add('tabheader__item_active');
 	
 	function hideTabContent() {
 		tabsContent.forEach(item => {
@@ -38,4 +35,60 @@ document.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
+
+	//Timer
+	const deadline = '2021-01-10';
+
+	function getTimeRemaining(endtime){
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(t / (1000 * 60 * 60 * 24)), // Получем скоько дней осталось до окончания
+			hours = Math.floor((t / (1000 * 60 * 60) % 24)), // Делим общее кол-во часов на 24 и берем остаток, который
+			// как раз и будет кол-вом оставшихся часов
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			seconds = Math.floor((t / 1000) % 60);
+		
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds	
+		};
+	}
+
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		}
+		else {
+			return num;
+		}
+	}
+
+	function setClock(selector, endtime) {
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock () {
+			const t = getTimeRemaining(endtime)
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+
+	setClock('.timer', deadline);
+
 });
